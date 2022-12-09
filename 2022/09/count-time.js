f42bcd0b7cb8b6bb618fa82fd89f6de50234ce2a
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-12-09 18:29:09 -0500

    feat(2022): Update challenge 09

--- 2022/09/count-time.js
+++ 2022/09/count-time.js
@@ -4,22 +4,7 @@
  * @returns number
  */
 export default function countTime (leds) {
-  let counter = 0
-  let flag = false
-
-  while (!flag) {
-    if (leds.every(led => led === 1)) {
-      flag = true
-      break
-    }
-
-    leds = leds.map((led, pos, array) => {
-      if (led === 1) return led
-      const prevLed = array[pos === 0 ? array.length - 1 : pos - 1]
-      return prevLed === 1 ? 1 : 0
-    })
-    counter++
-  }
-
-  return counter * 7
+  const array = leds.join('').split('1')
+  array[0] += array.pop()
+  return Math.max(...array.map(group => group.length)) * 7
 }
