1a55241f47f22a5d5aff456cf023943a76793a49
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-12-21 19:09:13 -0500

    refactor(2022): Update challenge day 21

--- 2022/21/print-table.js
+++ 2022/21/print-table.js
@@ -4,27 +4,27 @@
  * @returns string
  */
 export default function printTable (gifts) {
-  const maxGiftLength = Math
-    .max(...gifts.map(gift => gift.name.length), 4)
-  const maxQuantityLenght = Math
-    .max(...gifts.map(gift => gift.quantity.toString().length), 8)
-
-  const titleGift = 'Gift'.padEnd(maxGiftLength, ' ')
-  const titleQuantity = 'Quantity'.padEnd(maxQuantityLenght, ' ')
-
-  const header = [
-    '+'.repeat(maxGiftLength + maxQuantityLenght + 7),
-    `| ${titleGift} | ${titleQuantity} |`,
-    `| ${'-'.repeat(titleGift.length)} | ${'-'.repeat(titleQuantity.length)} |`
+  const [maxGift, maxQuantity] = [
+    Math.max(...gifts.map(({ name }) => name.length), 4),
+    Math.max(...gifts.map(({ quantity }) => `${quantity}`.length), 8)
   ]
 
+  const titleGift = 'Gift'.padEnd(maxGift, ' ')
+  const titleQuantity = 'Quantity'.padEnd(maxQuantity, ' ')
+
+  const header = (
+    `${'+'.repeat(maxGift + maxQuantity + 7)}\n` +
+    `| ${titleGift} | ${titleQuantity} |\n` +
+    `| ${'-'.repeat(maxGift)} | ${'-'.repeat(maxQuantity)} |`
+  )
+
   const body = gifts.map(({ name, quantity }) => {
-    const giftText = name.padEnd(maxGiftLength, ' ')
-    const quantityText = quantity.toString().padEnd(maxQuantityLenght, ' ')
+    const giftText = name.padEnd(maxGift, ' ')
+    const quantityText = `${quantity}`.padEnd(maxQuantity, ' ')
     return `| ${giftText} | ${quantityText} |`
   })
 
-  const footer = '*'.repeat(maxGiftLength + maxQuantityLenght + 7)
+  const footer = '*'.repeat(maxGift + maxQuantity + 7)
 
-  return [...header, ...body, footer].join('\n')
+  return [header, ...body, footer].join('\n')
 }
